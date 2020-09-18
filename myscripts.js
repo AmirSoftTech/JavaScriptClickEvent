e2c7cb8b82dfba5bf224ffbcb5aaf0ef15c8e7d3
Mohammad Amirul Islam <33347680+AmirSoftTech@users.noreply.github.com>
2020-09-18 14:57:08 +0200

    Added JavaScript new files

---
+++ myscripts.js
@@ -0,0 +1,17 @@
+function myMessage(){
+    var message = document.querySelector("#myId");
+    message.innerHTML="<h1>Hello I am from Bangladesh</h1>";
+    message.style.color="blue";
+    message.style.fontSize="30px";
+}
+
+
+function myImage(){
+    var images = document.querySelector("#mg");
+    images.src = "logo.jpg";
+    images.style.marginRight="30px";
+}
+
+
+
+
